7469c0e618b20b6cc71d0937686ae5faac8d9a7d
Tuan Tran <tuan.tran@ntq-solution.com.vn>
2018-04-16 17:47:26 +0700

    create login GUI

---
+++ QuanLiKhoHang/QuanLiKhoHang/Login.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace QuanLiKhoHang
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userNameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.userNameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.loginGroupBox = new System.Windows.Forms.GroupBox();
+            this.loginGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // userNameLabel
+            // 
+            this.userNameLabel.AutoSize = true;
+            this.userNameLabel.Location = new System.Drawing.Point(52, 50);
+            this.userNameLabel.Name = "userNameLabel";
+            this.userNameLabel.Size = new System.Drawing.Size(70, 15);
+            this.userNameLabel.TabIndex = 0;
+            this.userNameLabel.Text = "User Name";
+            this.userNameLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(59, 96);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(61, 15);
+            this.passwordLabel.TabIndex = 1;
+            this.passwordLabel.Text = "Password";
+            this.passwordLabel.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(154, 148);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(139, 31);
+            this.loginButton.TabIndex = 2;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            // 
+            // userNameTextBox
+            // 
+            this.userNameTextBox.Location = new System.Drawing.Point(154, 43);
+            this.userNameTextBox.Name = "userNameTextBox";
+            this.userNameTextBox.Size = new System.Drawing.Size(204, 21);
+            this.userNameTextBox.TabIndex = 3;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(154, 89);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(204, 21);
+            this.passwordTextBox.TabIndex = 4;
+            // 
+            // loginGroupBox
+            // 
+            this.loginGroupBox.Controls.Add(this.userNameTextBox);
+            this.loginGroupBox.Controls.Add(this.passwordLabel);
+            this.loginGroupBox.Controls.Add(this.loginButton);
+            this.loginGroupBox.Controls.Add(this.userNameLabel);
+            this.loginGroupBox.Controls.Add(this.passwordTextBox);
+            this.loginGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginGroupBox.Location = new System.Drawing.Point(53, 24);
+            this.loginGroupBox.Name = "loginGroupBox";
+            this.loginGroupBox.Size = new System.Drawing.Size(434, 224);
+            this.loginGroupBox.TabIndex = 5;
+            this.loginGroupBox.TabStop = false;
+            this.loginGroupBox.Text = "Login";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 313);
+            this.Controls.Add(this.loginGroupBox);
+            this.Name = "Login";
+            this.Text = "Hệ thống quản lí kho hàng";
+            this.loginGroupBox.ResumeLayout(false);
+            this.loginGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label userNameLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.TextBox userNameTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.GroupBox loginGroupBox;
+    }
+}
+
